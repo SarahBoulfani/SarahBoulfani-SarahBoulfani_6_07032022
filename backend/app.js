17ba252d3b057edda8026da74e7a895ccc5b7d35
Sarah Boulfani <boulfanisarah@gmail.com>
2022-03-14 15:01:26 +0100

    Ajout des variables d'environnement

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,6 @@
+//Imporet package dotenv pour utiliser les variables d'environnement
+require('dotenv').config();
+//console.log(process.env); // remove this after you've confirmed it working
 //importer express
 const express = require('express');
 //créer notre application en utilisant la méthode express ce qui permet de créer une application express
@@ -9,7 +12,7 @@ app.use(express.json());
 const mongoose = require('mongoose');
 
 //Connexion de l'API à notre base de données_____________________________________
-mongoose.connect('mongodb+srv://Sarah:cluster1-sarah2022@cluster0.in87l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://${process.env.BD_USERNAME}:${process.env.BD_SECRET_KEY}@${process.env.BD_CLUSTER_NAME}.mongodb.net/${process.env.BD_NAME}?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
